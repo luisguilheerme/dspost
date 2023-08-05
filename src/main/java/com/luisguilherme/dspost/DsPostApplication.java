30df62a2d93c764a6b8dd8ebbffa06a8ab30feef
luisguilheerme <lg.maroni@gmail.com>
2023-08-05 13:07:57 -0300

    Project created

---
+++ src/main/java/com/luisguilherme/dspost/DsPostApplication.java
@@ -0,0 +1,13 @@
+package com.luisguilherme.dspost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsPostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luisguilherme/dspost/DsPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisguilherme.dspost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
